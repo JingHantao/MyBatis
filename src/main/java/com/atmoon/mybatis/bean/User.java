99ddfe351fe9a482aca0af227992bee3d3d79914
Lost97 <1175049698@qq.com>
2018-12-16 22:56:29 +0800

    2018-12-16

---
+++ src/main/java/com/atmoon/mybatis/bean/User.java
@@ -0,0 +1,115 @@
+package com.atmoon.mybatis.bean;
+import java.util.Date;
+
+/**
+ * @auther: asuspc
+ * @Date: 2018/12/16 22:42
+ * @Description:
+ */
+public class User implements java.io.Serializable {
+
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private Date date;
+
+    private String created;
+
+    private String updated;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", date=" + date +
+                ", created='" + created + '\'' +
+                ", updated='" + updated + '\'' +
+                '}';
+    }
+}
--- src/test/java/com/atmoon/basetest/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.atmoon.basetest;
-
-/**
- * @auther: asuspc
- * @Date: 2018/12/10 23:13
- * @Description:
- */
-public class BaseTest {
-}
